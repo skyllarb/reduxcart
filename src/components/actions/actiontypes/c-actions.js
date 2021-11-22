0e44741ccb9a2edaa54517542f26574745b2a057
skyllarb <skyllarbyron@gmail.com>
2021-11-22 16:15:40 -0500

    react shop app basic

---
+++ src/components/actions/actiontypes/c-actions.js
@@ -0,0 +1,6 @@
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const SUBTRACT_QUANTITY = 'SUB_QUANTITY';
+export const ADD_QUANTITY = 'ADD_QUANTITY';
+export const ADD_SHIPPING = 'ADD_SHIPPNG';
+
